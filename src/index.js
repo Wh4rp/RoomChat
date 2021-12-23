e919ee1f2f1c6a6b7c70fecac22d584a35963ea4
Wh4rp <matiasfernandeztaipe@gmail.com>
2021-12-23 00:13:57 -0300

    first version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Buenas Express app!</h1>')
-});
+// setings
+app.set('port', 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 
-app.listen(3000, () => {
+// routes
+app.use(require('./routes/index'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(app.get('port'), () => {
   console.log('server started');
 });
